2a12cce0a7f5c2d26757a814fff182c630a332b2
Caio Castro <caio.castro3909@gmail.com>
2022-08-16 13:02:05 +0200

    Initial Commit

---
+++ RockPaper&Scissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaper&Scissors
+//
+//  Created by Caio Castro on 16/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaper&Scissors/RockPaper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaper_ScissorsApp.swift
+//  RockPaper&Scissors
+//
+//  Created by Caio Castro on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RockPaper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
